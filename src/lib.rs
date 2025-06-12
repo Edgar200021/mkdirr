654dbc92676119fb234f8ef08b5cfd4904eccc92
Edgar <edgarartur2000@mail.ru>
2025-06-12 10:12:13 +0400

    refactor: add changes in mode parsing and in create_directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use clap::{self, ArgAction, Command, arg, value_parser};
 use std::{
     error::Error,
-    fs::{self, Permissions, create_dir, create_dir_all, set_permissions},
+    fs::{Permissions, create_dir, create_dir_all, set_permissions},
     os::unix::fs::PermissionsExt,
+    path::Path,
+    process,
     str::FromStr,
 };
 
 pub type MyResult<T> = Result<T, Box<dyn Error>>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct Mode {
     user_read: bool,
     user_write: bool,
@@ -21,47 +23,29 @@ struct Mode {
     other_execute: bool,
 }
 
-impl Default for Mode {
-    fn default() -> Self {
-        Self {
-            user_read: false,
-            user_write: false,
-            user_execute: false,
-            group_read: false,
-            group_write: false,
-            group_execute: false,
-            other_read: false,
-            other_write: false,
-            other_execute: false,
-        }
-    }
-}
+impl FromStr for Mode {
+    type Err = String;
 
-impl TryFrom<String> for Mode {
-    type Error = String;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        let s = value.to_lowercase();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let str = s.to_lowercase();
         let mut mode = Mode::default();
 
-        if s.is_empty() {
+        if str.is_empty() {
             return Err(format!("Mode must be defined"));
         }
 
-        if s.contains("=") {
+        if str.contains("=") {
             for group_perms in s.split(",") {
-                let mut iter = group_perms.splitn(2, "=");
-                let class = iter.next().unwrap();
-                let perms = iter
-                    .next()
-                    .ok_or(format!("Invalid mode segment: {}", group_perms))?;
+                let (class, perms) = group_perms
+                    .split_once("=")
+                    .ok_or_else(|| format!("Invalid permission format: '{}'", group_perms))?;
 
                 if perms.chars().any(|c| !"rwx".contains(c)) {
                     return Err(format!("Invalid permissions in: {}", group_perms));
                 }
 
-                for p in perms.chars() {
-                    match (class, p) {
+                for perm in perms.chars() {
+                    match (class, perm) {
                         ("u", 'r') => mode.user_read = true,
                         ("u", 'w') => mode.user_write = true,
                         ("u", 'x') => mode.user_execute = true,
@@ -71,7 +55,7 @@ impl TryFrom<String> for Mode {
                         ("o", 'r') => mode.other_read = true,
                         ("o", 'w') => mode.other_write = true,
                         ("o", 'x') => mode.other_execute = true,
-                        _ => return Err(format!("Unknown class or perm: {}={}", class, p)),
+                        _ => return Err(format!("Unknown class or perm: {}={}", class, perm)),
                     }
                 }
             }
@@ -79,7 +63,7 @@ impl TryFrom<String> for Mode {
             Ok(mode)
         } else {
             if s.chars().any(|c| !"rwx".contains(c)) {
-                return Err(format!("Invalid mode: {}", value));
+                return Err(format!("Invalid mode: {}", s));
             }
 
             let read = s.contains('r');
@@ -98,13 +82,6 @@ impl TryFrom<String> for Mode {
                 other_execute: exec,
             })
         }
-    }
-}
-
-impl FromStr for Mode {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Mode::try_from(s.to_string())
     }
 }
 
@@ -186,30 +163,28 @@ pub fn read_config() -> MyResult<Config> {
 }
 
 fn create_directory(dir_name: &str, parents: bool, verbose: bool) -> MyResult<()> {
-    let metadata = fs::metadata(dir_name);
+    let path = Path::new(dir_name);
     let mut verbose_info = String::new();
 
     if parents {
-        if metadata.is_ok() {
+        if path.exists() {
             return Ok(());
         }
 
         if verbose {
-            let parts = dir_name.split("/").collect::<Vec<&str>>();
-
-            for (index, _) in parts.iter().enumerate() {
-                let (left, _) = parts.split_at(index + 1);
-                let full_path = left.join("/");
-                let metadata = fs::metadata(&full_path);
-
-                if metadata.is_ok() {
+            for ancestor in path.ancestors() {
+                if ancestor.exists() || ancestor.as_os_str() == "" {
                     continue;
                 }
-                verbose_info.push_str(format!("created directory '{}'\n", full_path).as_str());
+
+                verbose_info.insert_str(
+                    0,
+                    format!("created directory '{}'\n", ancestor.display()).as_str(),
+                );
             }
         }
 
-        create_dir_all(dir_name)?;
+        create_dir_all(path)?;
 
         if verbose && !verbose_info.is_empty() {
             print!("{}", verbose_info);
@@ -225,9 +200,13 @@ fn create_directory(dir_name: &str, parents: bool, verbose: bool) -> MyResult<()
 }
 
 pub fn run(config: &Config) -> MyResult<()> {
+    let mut exit_status = 0;
     for dir in config.dir_name.iter() {
         match create_directory(&dir, config.parents, config.verbose) {
-            Err(e) => eprintln!("cannot create directory `{dir}` {e}"),
+            Err(e) => {
+                exit_status = 1;
+                eprintln!("cannot create directory `{dir}` {e}");
+            }
             Ok(_) => {
                 if let Some(mode) = &config.mode {
                     set_permissions(dir, mode.into())?;
@@ -235,5 +214,10 @@ pub fn run(config: &Config) -> MyResult<()> {
             }
         }
     }
+
+    if exit_status == 1 {
+        process::exit(exit_status);
+    }
+
     Ok(())
 }
